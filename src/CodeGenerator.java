1b52f0490154f3e4fe8e5bd20f3451ad25c84599
Bogdan <bohdan00735@gmail.com>
2020-09-24 17:43:45 +0300

    masm settings

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -7,7 +7,15 @@ public class  CodeGenerator {
     private String asmCode;
     HashMap<String, AST> functionsAst;
 
-    private String masmTemplate ="";
+    private String masmTemplate =".586\n" +
+            ".model flat, stdcall\n" +
+            "option casemap :none ;to distinguish between uppercase and lowercase letters\n" +
+            "include \\masm32\\include\\windows.inc\n" +
+            "include D:\\masm32\\include\\kernel32.inc\n" +
+            "include D:\\masm32\\include\\user32.inc\n" +
+            "include module.inc\n" +
+            "includelib D:\\masm32\\lib\\kernel32.lib\n" +
+            "includelib D:\\masm32\\lib\\user32.lib";
 
     public CodeGenerator(AST ast, HashMap<String, AST> functionsAst) {
         this.ast = ast;
